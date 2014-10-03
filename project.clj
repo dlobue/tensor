7b001d5830b61dc805506a29f11d25d714563520
Dominic LoBue <dominic.lobue@gmail.com>
2014-10-02 17:04:50 -0700

    Add riemann as a def dependency

--- project.clj
+++ project.clj
@@ -3,4 +3,5 @@
   :url "https://github.com/dlobue/tensor"
   :license {:name "Apache License 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0.html"}
+  :profile {:dev {:dependencies [[riemann "0.2.6"]]}}
   :dependencies [[org.clojure/clojure "1.6.0"]])
