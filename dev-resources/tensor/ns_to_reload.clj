10d65397768da491e5e73d656649c9701084c6b0
George Jahad <github@blackbirdsystems.net>
2014-10-10 15:10:48 -0700

    added tests

---
+++ dev-resources/tensor/ns_to_reload.clj
@@ -0,0 +1,24 @@
+(ns tensor.ns-to-reload
+  (:require [riemann.streams :refer [sdo]]
+            [tensor.core :refer :all]))
+
+(def keyword-to-replace :original)
+
+(defmacro defmacro-and-fn
+  "Create identical streams with def-stream and def-stream-fn
+  env-keys param are the keywords to appear in the environment"
+  [name env-keys]
+  `(do
+     (def-stream ~(symbol name)
+       [{:keys ~env-keys :as env#} & children#]
+       (sdo
+        prn
+        #(swap! tensor.test-harness/test-atom conj ~@env-keys %)))
+     (def-stream-fn ~(str name "-fn")
+       (fn [{:keys ~env-keys :as env#} & children#]
+         (sdo
+          prn
+          #(swap! tensor.test-harness/test-atom conj ~@env-keys %))))))
+
+(defmacro-and-fn "test-no-env" [])
+(defmacro-and-fn "test-env" [event1 event2])
