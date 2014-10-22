3802cd8d6a236c78b8fc3cce5a5b4c6e45295163
Dominic LoBue <dominic.lobue@gmail.com>
2014-10-22 10:42:37 -0700

    bump version to 0.2.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject tensor "0.1.0-SNAPSHOT"
+(defproject tensor "0.2.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "https://github.com/dlobue/tensor"
   :license {:name "Apache License 2.0"
