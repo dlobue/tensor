0106053ff967bb1f948200a9e8fb6034bd15c6a2
Dominic LoBue <dominic.lobue@gmail.com>
2018-03-02 17:26:27 -0800

    upgrade deps

--- project.clj
+++ project.clj
@@ -3,12 +3,12 @@
   :url "https://github.com/dlobue/tensor"
   :license {:name "Apache License 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0.html"}
-  :profiles {:dev {:dependencies [[org.clojure/clojure "1.6.0"]
-                                  [riemann "0.2.6"]
-                                  [cloverage "1.0.6"]]
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.9.0"]
+                                  [riemann "0.3.0"]
+                                  [cloverage "1.0.10"]]
                    :resource-paths ["test/resources"]}}
   :plugins [[lein-cloverage "1.0.6"]]
-  :dependencies [[org.clojure/tools.logging "0.2.6"]
-                 [org.clojure/tools.macro "0.1.2"]
-                 [com.stuartsierra/dependency "0.1.1"]
-                 [medley "0.5.1"]])
+  :dependencies [[org.clojure/tools.logging "0.4.0"]
+                 [org.clojure/tools.macro "0.1.5"]
+                 [com.stuartsierra/dependency "0.2.0"]
+                 [medley "1.0.0"]])
